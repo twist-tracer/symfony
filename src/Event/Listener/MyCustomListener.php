2bcb975bf52081c883e710073cdf2095926e1c0f
Богданов Семен <twist.tracer@gmail.com>
2023-03-22 10:19:51 +0200

    <symfony> logger

--- src/Event/Listener/MyCustomListener.php
+++ src/Event/Listener/MyCustomListener.php
@@ -2,13 +2,17 @@
 
 namespace App\Event\Listener;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Event\ConsoleTerminateEvent;
 
 class MyCustomListener
 {
-    public function __invoke(ConsoleTerminateEvent $event): void
+    public function __construct(private readonly LoggerInterface $logger)
     {
-        dump('Console command finished');
     }
 
+    public function __invoke(ConsoleTerminateEvent $event): void
+    {
+        $this->logger->debug('Console command finished');
+    }
 }
